9da60e0f2733474a2b3a2c75b2f3df0f617c7e79
safilbhoraniya <130131016+safilbhoraniya@users.noreply.github.com>
2023-11-27 19:16:10 +0530

    module 3 se fundmaental of programming

---
+++ 5--LeapYearOrNot.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int y;
+    printf("Enter a year: ");
+    scanf("%d", &y);
+
+    
+    if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)) {
+        printf("%d leap year.\n", y);
+    } else {
+        printf("%d not leap year.\n", y);
+    }
+
+    return 0;
+}
